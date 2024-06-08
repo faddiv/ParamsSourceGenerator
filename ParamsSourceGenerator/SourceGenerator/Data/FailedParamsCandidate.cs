a14816dae60f143473ba6a16f3565f1a5630778c
faddiv <faddiv@gmail.com>
2024-06-08 17:52:04 +0200

    Add Equatable to ParamsCandidates

--- ParamsSourceGenerator/SourceGenerator/Data/FailedParamsCandidate.cs
+++ ParamsSourceGenerator/SourceGenerator/Data/FailedParamsCandidate.cs
@@ -1,13 +1,30 @@
 ﻿using Microsoft.CodeAnalysis;
+using System;
 using System.Collections.Generic;
 
 namespace Foxy.Params.SourceGenerator.Data
 {
-    internal class FailedParamsCandidate : ParamsCandidate
+    internal class FailedParamsCandidate : ParamsCandidate, IEquatable<FailedParamsCandidate?>
     {
         public override bool HasErrors => true;
 
         public required List<Diagnostic> Diagnostics { get; init; }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as FailedParamsCandidate);
+        }
+
+        public bool Equals(FailedParamsCandidate? other)
+        {
+            return other is not null &&
+                   EqualityComparer<List<Diagnostic>>.Default.Equals(Diagnostics, other.Diagnostics);
+        }
+
+        public override int GetHashCode()
+        {
+            return 244270639 + EqualityComparer<List<Diagnostic>>.Default.GetHashCode(Diagnostics);
+        }
     }
 }
 
