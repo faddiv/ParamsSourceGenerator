f3f81ac58ac1f07f1eaaa3f290b6026293259ff0
faddiv <faddiv@gmail.com>
2024-07-12 20:57:41 +0200

    Make change tracking benchmark.

--- ParamsSourceGenerator/PerformanceTest/Program.cs
+++ ParamsSourceGenerator/PerformanceTest/Program.cs
@@ -1,9 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
 using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
 using PerformanceTest;
 
 /*var c = new CodeGenerationBenchmark();
 c.CreateBaseSource();
 c.RunGenerator();*/
+/*
+var c = new ChangeTrackingBenchmark();
+await c.CreateBaseSource();
+c.OnlyOneFileChanges();
+*/
 
-BenchmarkRunner.Run<CodeGenerationBenchmark>();
+var config = new ManualConfig()
+    .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+    .AddValidator(JitOptimizationsValidator.FailOnError)
+    .AddLogger(ConsoleLogger.Default)
+    .AddColumnProvider(DefaultColumnProviders.Instance);
+
+BenchmarkRunner.Run<ChangeTrackingBenchmark>(config);
